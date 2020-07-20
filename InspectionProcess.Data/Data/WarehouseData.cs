a4a8fe655d692bbc0c8e7300caeb94fc65f945e1
chlwjddms0716 <63761322+chlwjddms0716@users.noreply.github.com>
2020-07-20 17:52:26 +0900

    ddd

---
+++ InspectionProcess.Data/Data/WarehouseData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InspectionProcess.Data
+{
+    class _
+    {
+    }
+}
--- InspectionProcess/Program.cs
+++ InspectionProcess/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevExpressKiller;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,9 +15,21 @@ namespace InspectionProcess
         [STAThread]
         static void Main()
         {
+
+            ProcessKiller.Instance.ShutDown += ProcessKiller_ShutDown;
+            ProcessKiller.Instance.Start(interval: 500);
+
+            Console.WriteLine("Press Enter to quit");
+            Console.ReadLine();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new );
+        }
+
+        private static void ProcessKiller_ShutDown(object sender, ProcessKiller.ShutDownEventArgs e)
+        {
+            Console.WriteLine($"[{e.Count}] at {DateTime.Now}");
         }
     }
 }
