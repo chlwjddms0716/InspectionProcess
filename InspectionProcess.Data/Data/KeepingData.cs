12e1b0cc08f3d7de8e588da42492ad64ab2ad8c0
chlwjddms0716 <63761322+chlwjddms0716@users.noreply.github.com>
2020-07-21 10:04:12 +0900

    dfdfdffdfd

--- InspectionProcess.Data/Data/KeepingData.cs
+++ InspectionProcess.Data/Data/KeepingData.cs
@@ -8,6 +8,22 @@ namespace InspectionProcess.Data
 {
     class KeepingData : EntityData<Keeping>
     {
-   
+        public Keeping Get(int warehouseId, int inspectionResultId)
+        {
+            InspectionProcessEntities context = CreateContext();
+
+            return context.Keepings.FirstOrDefault(a => a.WarehouseId == warehouseId && a.InspectionResultId == inspectionResultId);
+        }
+
+        public void Delete(int warehouseId, int inspectionResultId)
+        {
+            Keeping keeping = Get(warehouseId, inspectionResultId);
+
+            if (keeping == null)
+                return;
+
+            Delete(keeping);
+        }
+
     }
 }
